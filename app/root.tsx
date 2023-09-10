dff92f3ce182316c9949b94ea8456cb0ae5ad11d
turkerdev <48413508+turkerdev@users.noreply.github.com>
2023-09-10 22:48:57 +0300

    add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import { json, type LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,6 +6,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 import stylesheet from "./tailwind.css";
 
@@ -13,7 +15,20 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
+export const loader = async () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
+};
+
 export default function App() {
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
+  // useEffect(() => {
+  //   if (gaTrackingId?.length) {
+  //     // gtag.pageview(location.pathname, gaTrackingId);
+  //   }
+  // }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -23,6 +38,29 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-neutral-800 text-neutral-200">
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <p className="bg-yellow-400 text-black text-center text-sm">
           You will encounter lots of bugs.
         </p>
